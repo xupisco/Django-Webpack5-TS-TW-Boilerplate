8d8041c029d9a78e42649c812c8347d2bc9336d3
Leandro Voltolino <leandro@voltolino.com>
2024-02-24 10:07:21 -0300

    playing around

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         require('daisyui')
     ],
     daisyui: {
-        themes: ['light', 'dark', 'cupcake'], // false: only light + dark | true: all themes | array: specific themes like this ['light', 'dark', 'cupcake']
+        themes: ['light', 'dark', 'emerald'], // false: only light + dark | true: all themes | array: specific themes like this ['light', 'dark', 'cupcake']
         darkTheme: 'dark', // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
